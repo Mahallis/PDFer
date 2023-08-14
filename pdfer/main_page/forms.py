100b4b2b5fe21555f63aaab0f7149cdcc96738ef
Samir Tagiev <tseo62546@gmail.com>
2023-08-14 23:25:33 +0500

    fixed gitignore

--- pdfer/main_page/forms.py
+++ pdfer/main_page/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(label="File Name", max_length=50)
     file = forms.FileField(
         label="File upload")
